de39b2618840bbbb41103f2f8f0f82cf16c3e0ec
jordangaston <jordangaston1994@gmail.com>
2022-01-30 17:25:50 -0600

    Implement pickledblog

--- ds_from_scratch/raft/log.py
+++ ds_from_scratch/raft/log.py
@@ -1,16 +1,83 @@
-class Log:
+class PickleDbLog:
 
-    def __setitem__(self, key, value):
-        pass
+    def __init__(self, db):
+        self.db = db
+        self.len = 0
+        self.__init_log()
 
-    def __getitem__(self, item):
-        pass
+    def __setitem__(self, index, value):
+        if type(index) is not int:
+            raise ValueError('type of index must be int')
+
+        if index < 0 or index >= self.len:
+            raise IndexError('log index out of range')
+
+        self.db.dadd('log', (str(index), value))
+        self.db.dump()
+
+    def __getitem__(self, index):
+        if type(index) is int:
+            return self.__get_item(index)
+        elif type(index) is slice:
+            return self.__get_range(index)
+        else:
+            raise ValueError('type of index must be int or slice')
 
     def __iter__(self):
-        pass
+        return self.Iterator(self)
 
-    def append(self):
-        pass
+    def __len__(self):
+        return self.len
+
+    def append(self, *entries):
+        for entry in entries:
+            self.db.dadd('log', (self.len, entry))
+            self.len += 1
+        self.db.dump()
+
+    def __get_range(self, lslice):
+        start = lslice.start
+        stop = lslice.stop if lslice.stop is not None else self.len
+
+        if start < 0 or start >= self.len:
+            raise IndexError('slice start index out of range')
+
+        if start >= stop:
+            raise IndexError('slice start must be less than slice end')
+
+        if lslice.step and lslice.step != 1:
+            raise ValueError('slice step must equal 1')
+
+        values = []
+
+        for i in range(start, min(self.len, stop)):
+            values.append(self.db.dget('log', str(i)))
+
+        return values
+
+    def __get_item(self, index):
+        if index < 0 or index >= self.len:
+            raise IndexError('log index out of range')
+
+        return self.db.dget('log', str(index))
+
+    def __init_log(self):
+        if not self.db.exists('log'):
+            self.db.dcreate('log')
+        else:
+            self.len = len(self.db.dkeys('log'))
+
+    class Iterator:
+        def __init__(self, log):
+            self.curr = 0
+            self.log = log
+
+        def __next__(self):
+            if self.curr >= len(self.log):
+                raise StopIteration
+            entry = self.log[self.curr]
+            self.curr += 1
+            return entry
 
 
 class LogEntry:
